9769f2d58c5f7a2a67023419c4b3c2a31720c23d
NateBal <89489235+NateBal@users.noreply.github.com>
2022-02-17 14:24:16 -0800

    Stopwatch finish, and ghosts started

---
+++ Assets/Scripts/StopWatch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class StopWatch : MonoBehaviour
+{
+    public float timeStart;
+    public float newScore;
+    private TextMeshProUGUI StopwatchText;
+
+    bool timer = false;
+
+    void Start()
+    {
+        timeStart = 0;
+        timer = true;
+        StopwatchText = GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        if (timer)
+        {
+            timeStart += Time.deltaTime;
+            StopwatchText.text = Mathf.FloorToInt(timeStart).ToString();
+        }
+    }
+}
