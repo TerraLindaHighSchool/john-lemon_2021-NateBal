e340ce6b018dd9833e3e552060605bd1ae53a9c2
NateBal <89489235+NateBal@users.noreply.github.com>
2022-03-03 14:13:35 -0800

    John Lemon Complete

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+
+    private float currentPosition;
+    void Start()
+    {
+        currentPosition = transform.rotation.y;
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKey("left"))
+        {
+            transform.Rotate(45, currentPosition + 90, 0);
+        }
+
+        if (Input.GetKey("right"))
+        {
+            transform.Rotate(45, currentPosition - 90, 0);
+        }
+    }
+}
